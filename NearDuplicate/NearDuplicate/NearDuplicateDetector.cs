0aca44cf2566675ff87666adf2ab4746ef5e7d38
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2017-09-05 11:02:57 +0200

    NearDuplication

---
+++ NearDuplicate/NearDuplicate/NearDuplicateDetector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace NearDuplicate
+{
+    public class NearDuplicateDetector
+    {
+        private readonly int _singleLength;
+
+        public NearDuplicateDetector(int sideLength = 4)
+        {
+            _singleLength = sideLength;
+        }
+
+        public float JaccardSimilarity(string a, string b)
+        {
+            return JaccardSimilarity(Shinglefy(a), Shinglefy(b));
+        }
+
+        public float JaccardSimilarity(List<string[]> shingleA, List<string[]> shingleB)
+        {
+            var overlapping = shingleA.Where(stringse => shingleB.Any(a => a.SequenceEqual(stringse))).ToList();
+
+            float overlap = overlapping.Count;
+
+            var union = shingleA.Count + shingleB.Count - overlap;
+            
+            return overlap / union;
+        }
+
+        public bool NearDuplicate(string a, string b)
+        {
+
+            var aShingles = Shinglefy(a);
+            var bShingles = Shinglefy(b);
+            
+
+
+            return false;
+        }
+        
+        private List<string[]> Shinglefy(string s)
+        {
+            var words = s.Replace(".", "").Replace(",","").Split(' ').ToList();
+            
+            var shingles = new List<string[]>();
+
+            for (var i = 0; i < words.Count - _singleLength + 1; i++)
+            {
+                shingles.Add(words.GetRange(i, _singleLength).ToArray());
+            }
+
+            return shingles;
+        }
+    }
+}
---
+++ NearDuplicate/NearDuplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstString = "do not worry about your difficulties in mathematics.";
+            var secondString = "i would not worry about your difficulties, you can easily learn what is needed.";
+
+            var start = DateTime.Now;
+
+            var res = new NearDuplicateDetector(3).JaccardSimilarity(firstString, secondString);
+
+            var end = DateTime.Now;
+
+            Console.WriteLine($"Near Duplicate: {res:F}, {end - start}");
+            Console.ReadKey();
+        }
+    }
+}
