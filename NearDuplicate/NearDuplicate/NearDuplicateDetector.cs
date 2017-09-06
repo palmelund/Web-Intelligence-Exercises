4cd014f2f94dd79d7482fb2d858a50bc6783c5d4
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2017-09-06 14:37:35 +0200

    Added method for string permutation.

--- NearDuplicate/NearDuplicate/NearDuplicateDetector.cs
+++ NearDuplicate/NearDuplicate/NearDuplicateDetector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace NearDuplicate
@@ -32,6 +33,16 @@ namespace NearDuplicate
             return overlap / union;
         }
 
+        public float JaccardSimilarity(List<int> hashsetA, List<int> hashsetB)
+        {
+            var overlapping = hashsetA.Where(h => hashsetB.Any(g => h == g)).ToList();
+            float overlap = overlapping.Count;
+
+            var union = hashsetA.Count + hashsetB.Count - overlap;
+
+            return overlap / union;
+        }
+
         public float NearDuplicate(string a, string b)
         {
 
@@ -40,13 +51,73 @@ namespace NearDuplicate
 
             var hashesA = aShingles.Select(stringse => stringse.GetHashCode()).ToList();
             var hashesB = bShingles.Select(stringse => stringse.GetHashCode()).ToList();
+            
+            var rand = new Random();
+
+            var minA = new List<int>();
+            var minB = new List<int>();
+
+            var alg = HashAlgorithm.Create("System.Security.Cryptography.SHA512");
+            
 
 
+            for (int i = 0; i < 5; i++)
+            {
+                var h = rand.Next();
 
-            Console.WriteLine(hashesA.Min());
-            Console.WriteLine(hashesB.Min());
+                hashesA = hashesA.Select(s => int.Parse(Permute(s.ToString(), 0, h))).ToList();
+                hashesB = hashesB.Select(s => int.Parse(Permute(s.ToString(), 0, h))).ToList();
+                
+                minA.Add(hashesA.Min());
+                minB.Add(hashesB.Min());
+            }
 
-            return 0f;
+            Console.WriteLine("MinA");
+
+            foreach (var m in minA)
+            {
+                Console.WriteLine(m);
+            }
+
+            Console.WriteLine("MinB");
+
+            foreach (var m in minB)
+            {
+                Console.WriteLine(m);
+            }
+
+
+            return JaccardSimilarity(minA, minB);
+        }
+
+        private string Permute(string s, int l, int r)
+        {
+            r %= s.Length;
+
+            if (l == r)
+            {
+                // Console.WriteLine(s);
+            }
+            else
+            {
+                for (var i = l; i <= r; i++)
+                {
+                    s = Swap(s, l, i);
+                    s = Permute(s, l+1, r);
+                    s = Swap(s, l, i);
+                }
+            }
+
+            return s;
+        }
+
+        private string Swap(string s, int i, int j)
+        {
+            var charArray = s.ToCharArray();
+            var temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            return string.Concat(charArray);
         }
         
         private List<string[]> Shinglefy(string s)
